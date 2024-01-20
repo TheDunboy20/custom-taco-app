c928aa5b449d607ba05a2309ce4b224399aaf460
smam02 <smam02@vse.cz>
2024-01-20 21:50:16 +0100

    Initial commit

---
+++ src/main/java/com/taco/customtacoapp/CustomTacoAppApplication.java
@@ -0,0 +1,13 @@
+package com.taco.customtacoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomTacoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomTacoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taco/customtacoapp/CustomTacoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taco.customtacoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomTacoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
