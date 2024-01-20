a169f128084b825961d6b15ec4997801e9c8654d
smam02 <smam02@vse.cz>
2024-01-20 22:27:58 +0100

    add Taco and TacoOrder entities

---
+++ src/main/java/com/taco/customtacoapp/Taco.java
@@ -0,0 +1,11 @@
+package com.taco.customtacoapp;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
